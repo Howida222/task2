fc268243c3173a244cb7d27356f58bd87c3d04b0
Howida222 <wda192005@gmail.com>
2024-08-20 13:40:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").style.color = 
+"rgb(255,255,255)";
+document.getElementById("demo").style.backgroundColor=
+"blue";
+document.getElementById("demo").style.textAlign=
+"center";
